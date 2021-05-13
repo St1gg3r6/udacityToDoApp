8d57bc5b4ab1afc3fee82fa8c7fea2966e4fd66b
Steve Usher <31818975+St1gg3r6@users.noreply.github.com>
2021-05-13 11:56:41 +0100

    Lesson 6 Migrations

--- app.py
+++ app.py
@@ -1,24 +1,25 @@
 from flask import Flask, render_template, request, abort
 from flask.json import jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 import sys
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://stephenusher@localhost:5432/todoapp'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+
+migrate = Migrate(app, db)
 
 
 class Todo(db.Model):
     __tablename__ = 'todos'
     todoid = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
+    completed = db.Column(db.Boolean, nullable=False, default=False)
 
     def __repr__(self) -> str:
         return f'<Todo {self.todoid} {self.description}>'
-
-
-db.create_all()
 
 
 @app.route('/')
