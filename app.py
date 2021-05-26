145d8af259e6d869627de0bb8ee141e338abb671
Steve Usher <31818975+St1gg3r6@users.noreply.github.com>
2021-05-26 11:47:51 +0100

    Added List functions

--- app.py
+++ app.py
@@ -1,9 +1,25 @@
+from logging import error
+from typing import final
 from flask import Flask, render_template, request, url_for, redirect, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+import sys
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://stephenusher@localhost:5432/todoapp'
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
+class TodoList(db.Model):
+    __tablename__ = 'todolist'
+    todolistid = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), nullable=False)
+    completed = db.Column(db.Boolean, nullable=False, default=False)
+    todos = db.relationship('Todo', backref='list', lazy=True)
+
+    def __repr__(self):
+        return f'<Todolist {self.todolistid} {self.name} >'
 
 
 class Todo(db.Model):
@@ -11,12 +27,11 @@ class Todo(db.Model):
     todoid = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
     completed = db.Column(db.Boolean, nullable=False, default=False)
+    todolistid = db.Column(db.Integer, db.ForeignKey(
+        'todolist.todolistid'), nullable=False)
 
     def __repr__(self):
         return f'<Todo {self.todoid} {self.description} >'
-
-
-db.create_all()
 
 
 @app.route('/todos/create', methods=['POST'])
@@ -44,12 +59,14 @@ def create_todo_a():
     data = {}
     try:
         newdescription = request.get_json()['description']
-        newtodo = Todo(description=newdescription)
+        selectedList = request.get_json()['todolistid']
+        newtodo = Todo(description=newdescription, todolistid=selectedList)
         db.session.add(newtodo)
         db.session.commit()
         # Add the data to the dictionary to be sent back
         data['todoid'] = newtodo.todoid
         data['description'] = newtodo.description
+        data['todolistid'] = newtodo.todolistid
     except:
         error = True
         db.session.rollback()
@@ -63,12 +80,52 @@ def create_todo_a():
         return jsonify(data)
 
 
-@app.route('/todos/<todoid>/updateCompStatus', methods=['POST'])
+@app.route('/lists/create', methods=['POST'])
+def createNewList():
+    error = False
+    try:
+        newListName = request.form.get('listdescription', '')
+        newList = TodoList(name=newListName)
+        db.session.add(newList)
+        db.session.commit()
+    except:
+        error = True
+        db.session.rollback()
+    finally:
+        addedListID = newList.todolistid
+        db.session.close()
+    if not error:
+        return redirect(url_for('gettodos', listid=addedListID))
+
+
+@app.route('/lists/<listid>/completeList', methods=['POST'])
+def completeList(listid):
+    print('In the completeList function, listid: ', listid)
+    try:
+        completeStatus = request.get_json()['completed']
+        print('completeStatus:', completeStatus)
+        todoList = TodoList.query.get(listid)
+        print('todoList: ', todoList)
+        todos = Todo.query.filter_by(todolistid=listid).all()
+        print('todos: ', todos)
+        todoList.completed = completeStatus
+        for todo in todos:
+            todo.completed = completeStatus
+        db.session.commit()
+    except:
+        db.session.rollback
+        print(sys.exc_info())
+    finally:
+        db.session.close()
+    return redirect(url_for('gettodos', listid=listid))
+
+
+@ app.route('/todos/<todoid>/updateCompStatus', methods=['POST'])
 def updateTodoCompStatus(todoid):
-    print('todoid = ', todoid)
     try:
         completeStatus = request.get_json()['completed']
         todoItem = Todo.query.get(todoid)
+        print('todoItem: ', todoItem)
         todoItem.completed = completeStatus
         db.session.commit()
     except:
@@ -79,7 +136,29 @@ def updateTodoCompStatus(todoid):
     return 'Record Updated'
 
 
-@app.route('/todos/<todoid>/deleteTodo', methods=['DELETE'])
+@app.route('/lists/<listid>/deleteList', methods=['DELETE'])
+def deleteList(listid):
+    try:
+        print("TRYING TO DELETE LIST")
+        todoItems = Todo.query.filter_by(todolistid=listid).all()
+        for todoItem in todoItems:
+            db.session.delete(todoItem)
+
+        listItem = TodoList.query.get(listid)
+        db.session.delete(listItem)
+
+        db.session.commit()
+        print('GOT TO LINE AFTER COMMIT')
+    except:
+        db.session.rollback()
+        print('ROLLED BACK SESSION IN DELETE LIST PROCESS')
+    finally:
+        db.session.close()
+    # return redirect(url_for('gettodos', listid=listid))
+    return 'List deleted'
+
+
+@ app.route('/todos/<todoid>/deleteTodo', methods=['DELETE'])
 def deleteTodo(todoid):
     print('todoid = ', todoid)
     try:
@@ -93,12 +172,29 @@ def deleteTodo(todoid):
     return 'Record Deleted'
 
 
-@app.route('/')
+# @app.route('/')  # Initial index function before the parent lists was created
+# def index():
+#     # 'data' is the variable to pass to the HTML template
+#     # which contains the data that needs to be processed
+#     # by the jinja code in the HTML file.
+#     return render_template('index.html', data=Todo.query.order_by('todoid').all())
+
+
+# Index function now that the parent list model has been implemented
+@ app.route('/lists/<listid>')
+def gettodos(listid):
+    print('RUNNING gettodos with listid:', listid)
+    return render_template('index.html',
+                           list=TodoList.query.order_by('todolistid').all(),
+                           data=Todo.query.filter_by(
+                               todolistid=listid).order_by('todoid').all(),
+                           selected=listid)
+
+
+@ app.route('/')
 def index():
-    # 'data' is the variable to pass to the HTML template
-    # which contains the data that needs to be processed
-    # by the jinja code in the HTML file.
-    return render_template('index.html', data=Todo.query.order_by('todoid').all())
+    # New index function redirects to the gettodos function with default list 1 selected
+    return redirect(url_for('gettodos', listid=1))
 
 
 if __name__ == '__main__':
--- static/js/script.js
+++ static/js/script.js
@@ -1,62 +1,30 @@
+syncSelectedList();
+listsActionLoad();
+todosActionLoad();
 document.querySelector('#form_async').onsubmit = function(e) {
-    // Prevent the browser from refreshing the page on submit
+    createTodo(e);
+}
+console.log('RELOADING PAGE');
+
+function createTodo(e) {
     e.preventDefault();
-    // Send the request to the server, 
-    // defining the method type, the body for the data to be sent
-    // and headers to define how the data is being sent.
+    const selectedList = document.querySelector('#selected').innerHTML;
+    const description = document.querySelector('#description_a').value;
+    const body = JSON.stringify({'description': description, 'todolistid': selectedList});
     fetch('/todos/create_a', {
         method: 'POST',
-        body: JSON.stringify({
-            'description': document.querySelector('#description_a').value
-        }),
+        body: body,
         headers: {
             'Content-Type': 'application/json'
         }
     })
-    // What to do when the response from the server has been received...
     .then(function(response) {
-        return response.json();
+        return response.json();        
     })
-    // Process the json response received from the first 'then', ie need to update the DOM...
     .then(function(jsonResponse) {
-        console.log(jsonResponse);
-        // The unordered list for the todos
-        const todoitems = document.querySelector('#todoitems');
-        // Create a new list item for the new todo
-        const todoitem = document.createElement('li');
-        todoitem.className = 'todoitem';
-        // Create a new completion checkbox for the new todo
-        const todochkbx = document.createElement('input');
-        // type="checkbox" name="complete" id="complete" data-id="{{ d.todoid }}"
-        todochkbx.type = 'checkbox';
-        todochkbx.name = 'complete';
-        todochkbx.id = 'complete';
-        todochkbx.className = 'check-completed';
-        todochkbx.dataset['id'] = jsonResponse['todoid'];
-        todochkbx.onchange = function(e) {
-            chkCompleteAction(e)
-        }
-        // Add the text for the new todo to the list item
-        todospan = document.createElement('span');
-        todospan.className = 'todotext';
-        todospan.innerHTML = jsonResponse['description'];
-        // Add delete button for the new todo to the list item
-        delbtn = document.createElement('button');
-        // <button type="submit" id="delete-todo" class="delete-todo" data-id="{{ d.todoid }}">&cross;</button>
-        delbtn.type = 'submit';
-        delbtn.id = 'delete-todo';
-        delbtn.className = 'delete-todo';
-        delbtn.dataset['id'] = jsonResponse['todoid'];
-        delbtn.innerHTML = '&cross;';
-        delbtn.onclick = function(e) {
-            deleteTodo(e)
-        };
-        // Append the new list item to the unordered list of todos
-        todoitem.appendChild(todochkbx);
-        todoitem.appendChild(todospan);
-        todoitem.appendChild(delbtn);
-        todoitems.appendChild(todoitem);
-        // Clear text from the text box ready for a new entry
+        const todoItemList = document.querySelector('#todoitems');
+        const newTodoItem = createNewTodoItemElement(jsonResponse);
+        todoItemList.appendChild(newTodoItem);
         document.querySelector('#description_a').value = '';
         document.querySelector('#error').className = 'hidden';
     })
@@ -65,27 +33,122 @@ document.querySelector('#form_async').onsubmit = function(e) {
     })
 }
 
-// Assign the click action to all of the checkboxes and delete buttons
-// Get all checkboxes and delete buttons into arrays
-const chkCompletes = document.querySelectorAll('#complete');
-const delTodos = document.querySelectorAll('#delete-todo');
-// Loop through them to assign the function
-for (let i = 0; i < chkCompletes.length; i++) {
-    const chkComplete = chkCompletes[i];
-    const delTodo = delTodos[i];
-    chkComplete.onchange = function(e) {
-        chkCompleteAction(e)
-    }
-    delTodo.onclick = function(e) {
-        deleteTodo(e)
+function createNewTodoItemElement(jsonResponse) {
+    const newTodoItem = document.createElement('li');
+    const newTodoID = jsonResponse['todoid'];
+    const newTodoDescription = jsonResponse['description'];
+    newTodoItem.className = 'todoitem';
+    const newTodoCheckbox = createNewToDoCheckbox(newTodoID);
+    const newTodoSpan = createNewTodoDescription(newTodoDescription);
+    const newTodoDeleteButton = createNewTodoDeleteButton(newTodoID);
+    newTodoItem.appendChild(newTodoCheckbox);
+    newTodoItem.appendChild(newTodoSpan);
+    newTodoItem.appendChild(newTodoDeleteButton);
+    return newTodoItem;
+}
+
+function createNewToDoCheckbox(todoid){
+    const newTodoCheckbox = document.createElement('input');
+    newTodoCheckbox.type = 'checkbox';
+    newTodoCheckbox.name = 'complete';
+    newTodoCheckbox.id = 'complete';
+    newTodoCheckbox.className = 'check-completed';
+    newTodoCheckbox.dataset['id'] = todoid;
+    newTodoCheckbox.onchange = function(e) { chkCompleteAction(e) };
+    return newTodoCheckbox;
+}
+function createNewTodoDescription(description){
+    const newTodoSpan = document.createElement('span');
+    newTodoSpan.className = 'todotext';
+    newTodoSpan.innerHTML = description;
+    return newTodoSpan;
+}
+function createNewTodoDeleteButton(todoid){
+    const newTodoDeleteButton = document.createElement('button');
+    newTodoDeleteButton.type = 'submit';
+    newTodoDeleteButton.id = 'delete-todo';
+    newTodoDeleteButton.className = 'delete-todo';
+    newTodoDeleteButton.dataset['id'] = todoid;
+    newTodoDeleteButton.innerHTML = '&cross;';
+    newTodoDeleteButton.onclick = function(e) { deleteTodo(e) };
+    return newTodoDeleteButton;
+}
+
+function listsActionLoad() {
+    const chkCompletes = document.querySelectorAll('#listcomplete');
+    const delLists = document.querySelectorAll('#delete-list');
+    for (let i = 0; i < chkCompletes.length; i++) {
+        const chkComplete = chkCompletes[i];
+        const delList = delLists[i];
+        chkComplete.onchange = function(e) { chkListCompleteAction(e) }
+        delList.onclick = function(e) { deleteList(e) }
     }
 }
 
+function chkListCompleteAction(e) {
+    const compStatus = e.target.checked;
+    console.log('compStatus: ' + compStatus);
+    const listid = e.target.dataset['id'];
+    fetch('../lists/' + listid + '/completeList', {
+        method: 'POST',
+        body: JSON.stringify({
+            'completed': compStatus
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(function(){
+        document.querySelector('#listerror').className = 'hidden';
+        // location.reload();
+        const todos = document.querySelectorAll('#complete');
+        for (let i = 0; i < todos.length; i++) {
+            todos[i].checked = compStatus;
+        }
+    })
+    .catch(function(){
+        document.querySelector('#listerror').className = '';
+    })
+}
+
+function deleteList(e) {
+    const listid = e.target.dataset['id'];
+    fetch('../lists/' + listid + '/deleteList', {
+        method: 'DELETE',
+    })
+    .then(function() {
+        const listitem = e.target.parentElement;
+        listitem.remove();
+        // const todos = document.querySelectorAll('#todoitems')
+        // todos.remove();
+        location.reload();
+        document.querySelector('#listerror').className = 'hidden';
+    })
+    .catch(function(){
+        document.querySelector('#listerror').className = '';
+    })
+}
+
+function todosActionLoad() {
+    // TODOS
+    // Assign the click action to all of the checkboxes and delete buttons
+    // Get all checkboxes and delete buttons into arrays
+    const chkCompletes = document.querySelectorAll('#complete');
+    const delTodos = document.querySelectorAll('#delete-todo');
+    // Loop through them to assign the function
+    for (let i = 0; i < chkCompletes.length; i++) {
+        const chkComplete = chkCompletes[i];
+        const delTodo = delTodos[i];
+        chkComplete.onchange = function(e) { chkCompleteAction(e) }
+        delTodo.onclick = function(e) { deleteTodo(e) }
+    }
+}
+
+
 function chkCompleteAction(e) {
-    console.log('event', e);
     const compStatus = e.target.checked;
     const todoid = e.target.dataset['id'];
-    fetch('todos/' + todoid + '/updateCompStatus', {
+    fetch('../todos/' + todoid + '/updateCompStatus', {
         method: 'POST',
         body: JSON.stringify({
             'completed': compStatus
@@ -103,9 +166,8 @@ function chkCompleteAction(e) {
 }
 
 function deleteTodo(e) {
-    console.log('event', e);
     const todoid = e.target.dataset['id'];
-    fetch('todos/' + todoid + '/deleteTodo', {
+    fetch('../todos/' + todoid + '/deleteTodo', {
         method: 'DELETE',
     })
     .then(function() {
@@ -116,4 +178,17 @@ function deleteTodo(e) {
     .catch(function() {
         document.querySelector('#error').className = '';
     })
+}
+
+function syncSelectedList() {
+    const selected = document.querySelector('#selected');
+    const list = document.querySelectorAll('#listitem');
+    for (let i = 0; i < list.length; i++) {
+        if (list[i].dataset['id'] == selected.innerHTML) {
+            list[i].className = "todoitem selected";
+            document.querySelector('#selectedlistname').innerHTML = list[i].querySelector('#listname').innerHTML
+        } else {
+            list[i].className = "todoitem";
+        }
+    }
 }
